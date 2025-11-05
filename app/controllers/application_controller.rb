fada24ddd7b2938eeed47828108f124708c55276
Yuyakun0316 <3.16.yuyakun@gmail.com>
2025-11-05 17:51:25 +0900

    emailとpassword以外の値も保存できるように追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  # 新規登録・アカウント更新時に許可するパラメータを追加
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :occupation, :position])
+  end
 end
